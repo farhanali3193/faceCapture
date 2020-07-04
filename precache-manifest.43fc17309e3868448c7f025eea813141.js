400e00e212a1f5dd6fbf61b100dde7342ec88592
farhanali3193 <61304043+farhanali3193@users.noreply.github.com>
2020-07-04 02:31:18 -0700

    Updates

---
+++ precache-manifest.43fc17309e3868448c7f025eea813141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15a4f5722e6a1e09297af703219cba1",
+    "url": "/faceCapture/index.html"
+  },
+  {
+    "revision": "07e2e9128af80a3fcb3c",
+    "url": "/faceCapture/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "83246e3299ae2791ecf0",
+    "url": "/faceCapture/static/css/main.6bf00475.chunk.css"
+  },
+  {
+    "revision": "07e2e9128af80a3fcb3c",
+    "url": "/faceCapture/static/js/2.8d149484.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceCapture/static/js/2.8d149484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83246e3299ae2791ecf0",
+    "url": "/faceCapture/static/js/main.f8a8de60.chunk.js"
+  },
+  {
+    "revision": "2236af67f8cd2c9c984b",
+    "url": "/faceCapture/static/js/runtime-main.638c6d07.js"
+  }
+]);
